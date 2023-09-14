e6b8136c54c91bb2c4ae9f643abd73c947e02d21
shibedrill <53824200+shibedrill@users.noreply.github.com>
2023-09-14 16:06:44 -0400

    Linux PCI, helpers, fixed class int size

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod backend;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_pci_listing() {
+        let device_list = crate::backend::get_pci_list();
+        for device in device_list {
+            println!("{:#?}", device);
+        }
+    }
+}
