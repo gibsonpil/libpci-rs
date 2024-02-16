921fd991d3a72cb9ed51da7e5d1999564b5e69dc
Gibson Pilconis <gibsonpil@protonmail.com>
2024-02-16 11:22:05 -0500

    fix up pt. 1

--- src/backend/c/stub.c
+++ src/backend/c/stub.c
@@ -33,12 +33,12 @@
 
 pci_device_stack_t get_pci_stack(void) {
     pci_device_stack_t result = create_pci_device_stack();
-    printf("Got call to stub function get_pci_stack.");
+    fprintf(stderr, "Got call to stub function get_pci_stack.");
     return result;
 }
 
 pci_device_t get_pci_by_id(uint16_t vendor, uint16_t device) {
     pci_device_t result;
-    printf("Got call to stub function get_pci_by_id.");
+    fprintf(stderr, "Got call to stub function get_pci_by_id.");
     return result;
 }
