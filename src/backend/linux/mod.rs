0299a4e157ef3dd6daf709db548e34de406fd061
shibedrill <53824200+shibedrill@users.noreply.github.com>
2024-03-23 01:41:06 -0400

    fixed stupid linux macro

--- src/backend/linux/mod.rs
+++ src/backend/linux/mod.rs
@@ -28,27 +28,41 @@
 use crate::backend::all_fields_available;
 use crate::pci::*;
 use std::fs::*;
+use std::num::ParseIntError;
 
 // ahaha this particular code is by Shibe Drill
 
-/// Internal macro to get an integer of a specific type from a file in a
-/// directory. The integer type must implement from_str_radix().
-/// The path must be specified as a valid Path, and the attribute should
-/// be a string representing a filename.
-/// TODO: Please make this a generic.
-macro_rules! get_pci_device_attribute {
-    ($t:ty, $dir:expr, $attribute:expr) => {{
-        let dir_usable = match $dir {
-            Ok(f) => f,
-            Err(_) => {
-                return Err(PciEnumerationError::ReadDirectory);
+// look ma! macros!
+macro_rules! impl_trait_for_types {
+    ($trait_name:ident for $($type:ty),*) => {
+        $(
+            impl $trait_name for $type {
+                fn from_str_radix(src: &str, radix: u32) -> Result<Self, ParseIntError> {
+                    Self::from_str_radix(src, radix)
+                }
             }
-        };
+        )*
+    };
+}
+
+trait FromStrRadix { 
+    fn from_str_radix(src: &str, radix: u32) -> Result<Self, ParseIntError> where Self: Sized;
+}
+
+// WHY IS THIS NOT IN THE STANDARD LIBRARY
+// I will cry if they don't give this to us.
+impl_trait_for_types!(FromStrRadix for u8, u16, u32);
+
+/// Internal function to get a PCI device attribute of a certain integer size,
+/// from a file with a certain name, given the directory the file is in.
+/// T must be in the list above where I had to MANUALLY IMPL MY OWN TRAIT so I 
+/// could genericize this function.
+fn get_pci_device_attribute<T>(dir: &DirEntry, attribute: &str) -> Result<T, PciEnumerationError> where T: FromStrRadix {
 
         let file_contents = read_to_string(format!(
             "{}/{}",
-            dir_usable.path().to_string_lossy(),
-            $attribute
+            dir.path().to_string_lossy(),
+            attribute
         ))?;
         let input_string = if let Some(stripped) = file_contents.strip_prefix("0x") {
             stripped
@@ -56,8 +70,7 @@ macro_rules! get_pci_device_attribute {
             &file_contents
         }
         .trim();
-        <$t>::from_str_radix(input_string, 16)
-    }};
+        Ok(T::from_str_radix(input_string, 16)?)
 }
 
 /// Primary Linux backend functionality. Iterates through /sys/bus/pci/devices
@@ -70,31 +83,30 @@ pub fn _get_pci_list() -> Result<Vec<PciDeviceHardware>, PciEnumerationError> {
     let mut device_list: Vec<PciDeviceHardware> = Vec::new();
 
     for directory in read_dir("/sys/bus/pci/devices/")? {
+        let dir_unwrapped = directory?;
         // Class contains multiple items: class, subclass, and the programming
         // interface.
-        let class_code = get_pci_device_attribute!(u32, &directory, "class")?;
+        let class_code: u32 = get_pci_device_attribute(&dir_unwrapped, "class")?;
 
         device_list.push(PciDeviceHardware {
             address: PciDeviceAddress::try_from(
                 // try_from parses an address in the 0000:00:00.0 format
-                directory
-                    .as_ref()
-                    .unwrap()
+                dir_unwrapped
                     .file_name()
                     .into_string()
-                    .unwrap(),
+                    .unwrap() // How can we get rid of this
             )
             .ok(), // TODO: delete this when we change from an
             // Option<PciDeviceAddress> to a Result<PciDeviceAddress,
             // PciInformationError>
-            vendor_id: get_pci_device_attribute!(u16, &directory, "vendor")?, // Vendor ID
-            device_id: get_pci_device_attribute!(u16, &directory, "device")?, // Device ID
-            subsys_device_id: get_pci_device_attribute!(u16, &directory, "subsystem_device")?, // Subsystem Device ID
-            subsys_vendor_id: get_pci_device_attribute!(u16, &directory, "subsystem_vendor")?, // Subsystem Vendor ID
+            vendor_id: get_pci_device_attribute(&dir_unwrapped, "vendor")?, // Vendor ID
+            device_id: get_pci_device_attribute(&dir_unwrapped, "device")?, // Device ID
+            subsys_device_id: get_pci_device_attribute(&dir_unwrapped, "subsystem_device")?, // Subsystem Device ID
+            subsys_vendor_id: get_pci_device_attribute(&dir_unwrapped, "subsystem_vendor")?, // Subsystem Vendor ID
             class: ((class_code >> 16) & 0xFF) as u8, // Class
             subclass: ((class_code >> 8) & 0xFF) as u8, // Subclass
             programming_interface: (class_code & 0xFF) as u8, // Programming Interface
-            revision_id: get_pci_device_attribute!(u8, &directory, "revision")?, // Revision ID
+            revision_id: get_pci_device_attribute(&dir_unwrapped, "revision")?, // Revision ID
         })
     }
 
